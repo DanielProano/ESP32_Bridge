de413ebbf38a19e2b60281d8cea2f4edfe80b932
Danny <179722984+DanielProano@users.noreply.github.com>
2026-09-03 10:16:59 -0400

    crsf no longer going through esp32

--- include/bridge.h
+++ include/bridge.h
@@ -15,39 +15,21 @@
 #define STM32_BAUD      921600
 #define STM32_UART_BUF_SIZE 512
 
-#define CRSF_UART       UART_NUM_2
-#define CRSF_RX_PIN     18
-#define CRSF_BAUD       420000
-#define CRSF_UART_BUF_SIZE  256
-
 #define TCP_PORT        8080
 #define WIFI_SSID       "Dragonfly"
 #define WIFI_PASS       "dragonfly123"
 
 #define LAPTOP_QUEUE_DEPTH       4
-#define CRSF_FAILSAFE_TIMEOUT_MS 500
 #define STM32_LINK_TIMEOUT_MS    1000
-#define CRSF_MUTEX_TIMEOUT_MS    10
 #define STM32_UART_MUTEX_TIMEOUT_MS 10
 #define CLIENT_SOCK_MUTEX_TIMEOUT_MS 10
 #define STM32_UART_READ_TIMEOUT_MS   50
-#define CRSF_UART_READ_TIMEOUT_MS    50
-
-#define CRSF_SYNC_BYTE                     0xC8
-#define CRSF_MAX_FRAME_SIZE                64
-#define CRSF_FRAMETYPE_RC_CHANNELS_PACKED  0x16
 
 #define BRIDGE_TASK_STACK_SIZE      4096
 
-#define CRSF_TO_STM32_TASK_PRIORITY   6
 #define STM32_TO_LAPTOP_TASK_PRIORITY 5
 #define QUEUE_TO_STM32_TASK_PRIORITY  4
 #define TCP_SERVER_TASK_PRIORITY      3
-
-typedef struct {
-    RC_CHANNELS_PAYLOAD data;
-    uint32_t last_update_ticks;
-} CRSF_SLOT;
 
 typedef struct {
     uint8_t msg_id;
@@ -57,21 +39,15 @@ typedef struct {
 
 extern int g_client_sock;
 extern QueueHandle_t g_cmd_queue;
-extern CRSF_SLOT g_crsf_slot;
 extern SemaphoreHandle_t g_client_sock_mutex;
 extern SemaphoreHandle_t g_stm32_uart_mutex;
-extern SemaphoreHandle_t g_crsf_mutex;
 
 void wifi_init(void);
 void uart_init_stm32(void);
-void uart_init_crsf(void);
 void tcp_server_task(void *pvParameters);
 void queue_to_stm32_task(void *pvParameters);
 void stm32_to_laptop_task(void *pvParameters);
-void crsf_to_stm32_task(void *pvParameters);
 void bridge_to_stm32(uint8_t msg_id, const uint8_t *payload, uint8_t payload_len);
 void bridge_to_laptop(const FRAME *frame);
-void crsf_to_stm32(const RC_CHANNELS_PAYLOAD *rc);
-bool crsf_is_stale(void);
 
 #endif
--- main/main.c
+++ main/main.c
@@ -15,12 +15,10 @@ void app_main(void)
     oled_init();
     wifi_init();
     uart_init_stm32();
-    uart_init_crsf();
 
     g_cmd_queue = xQueueCreate(LAPTOP_QUEUE_DEPTH, sizeof(STM32_CMD));
     g_client_sock_mutex = xSemaphoreCreateMutex();
 
-    xTaskCreate(crsf_to_stm32_task, "crsf_to_stm32", BRIDGE_TASK_STACK_SIZE, NULL, CRSF_TO_STM32_TASK_PRIORITY, NULL);
     xTaskCreate(stm32_to_laptop_task, "stm32_to_laptop", BRIDGE_TASK_STACK_SIZE, NULL, STM32_TO_LAPTOP_TASK_PRIORITY, NULL);
     xTaskCreate(queue_to_stm32_task, "queue_to_stm32", BRIDGE_TASK_STACK_SIZE, NULL, QUEUE_TO_STM32_TASK_PRIORITY, NULL);
     xTaskCreate(tcp_server_task, "tcp_server", BRIDGE_TASK_STACK_SIZE, NULL, TCP_SERVER_TASK_PRIORITY, NULL);
